b8a72055b612f77ab22d4b363d7b01ce6bd9812f
Levi <levifernandes@usp.br>
2019-03-22 13:16:55 -0300

    Change from pointer to array

--- grafo.c
+++ grafo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <malloc.h>
 
 #define MAX 100
 
@@ -15,8 +16,8 @@ typedef struct vertice{
     char cor; //b é branco, c é cinza e p é preto
     int ordem;
     int min;
-
-    struct vertice* pai;
+    int pai;
+    
     NO* cab;
 }VERTICE;
 
@@ -26,7 +27,7 @@ typedef struct{
 }GRAFO;
 
 GRAFO* criarGrafo(int numerodeVertices){
-    GRAFO* gr = (GRAFO*) malloc (sizeof(GRAFO));
+    GRAFO* gr = (GRAFO*) malloc(sizeof(GRAFO));
     gr->vertices = numerodeVertices;
 
     for(int i = 1; i <= numerodeVertices; i++)
@@ -40,24 +41,27 @@ void adicionarAresta(GRAFO* g, int v, int u){
 }
 
 
-void buscaBiconexo(GRAFO* g, VERTICE* v, int* contador){
-    v->min = v->ordem;
-    NO* p = v->cab;
+void DFS(VERTICE* vertice, int v, int* contador){
+    vertice[v].min = vertice[v].ordem;
+    NO* p = vertice[v].cab;
+    
     while(p){
-        VERTICE* u = &(g->A[p->numeroDoVertice]);
-        if(u->cor == -1){
-            u->cor = 'c';
-            u->pai = v;
-            u->ordem = ++(*contador);
+        int u = p->numeroDoVertice;
+        if(vertice[u].cor == -1){
+
+            vertice[u].cor = 'c';
+            vertice[u].pai = v;
+            vertice[u].ordem = ++(*contador);
             
-            buscaBiconexo(g, u, contador);
+            DFS(vertice, u, contador);
         };
 
-        if (v->min > u->min && u != v->pai) 
-            v->min = u->min;
+        if (vertice[v].min > vertice[u].min && u != v->pai) 
+            vertice[v].min = vertice[u].min;
+
         p = p->prox;
     }
-    v->cor = 'p';
+    vertice[v].cor = 'p';
 };
 
 
@@ -67,22 +71,26 @@ bool ehBiconexo(GRAFO* g, char* arestaCritica){
     int contador = 1;
 
 
+    VERTICE* vertice = g->A;
+
     //resetar grafo
     for(int i = 1; i <= g->vertices; i++){
-        VERTICE* atual = &(g->A[i]);
+        VERTICE atual = vertice[i];
         
-        atual->cor = 'b';
-        atual->pai = NULL;
-        atual->min = 0;
-        atual->ordem = 0;
+        atual.cor = 'b';
+        atual.pai = 0;
+        atual.min = 0;
+        atual.ordem = 0;
     }
 
 
-    VERTICE* v = &(g->A[1]);
-    v->cor = 'c';
-    v->ordem = contador; 
-    buscaBiconexo(g, v, &contador);
-    v->cor = 'p';
+    int v = 1;
+    vertice[v].cor = 'c';
+    vertice[v].ordem = contador; 
+
+    DFS(vertice, v, &contador);
+    
+    v.cor = 'p';
 
     if (contador < vertices){ 
         printf ("NAO EH CONEXO");
@@ -92,9 +100,8 @@ bool ehBiconexo(GRAFO* g, char* arestaCritica){
     int j = 0;
     //o primeiro elemento sempre terá o minimo e ordem iguais
     for (int i = 2; i <= vertices; i++){
-        VERTICE v = g->A[i];
-        if (v.min >= v.ordem){
-            arestaCritica[j] = i + '-' + v.pai->numero;
+        if (vertice[v].min >= vertice[v].ordem){
+            arestaCritica[j] = i + '-' + v.pai;
             j++;
         }; 
     }
