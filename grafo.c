ac77645ac9095b09400297ea839546560771877b
Levi <levifernandes@usp.br>
2019-03-24 09:00:44 -0300

    Teste funcionando

--- grafo.c
+++ grafo.c
@@ -11,7 +11,7 @@ typedef struct adj{
 }NO;
 
 typedef struct vertice{
-
+    int numero;
     char cor; //b é branco, c é cinza e p é preto
     int ordem;
     int min;
@@ -25,13 +25,30 @@ typedef struct{
     VERTICE* A;//array de vertices
 }GRAFO;
 
+
+void printar(GRAFO* g){
+    for(int i = 1; i <= g->vertices; i++){
+        printf("Vertice %i tem adjacencia com", g->A[i].numero);
+        NO* p = g->A[i].cab;
+        while(p){
+            printf(" v%i", p->numeroDoVertice);
+            p = p->prox;
+        }
+        printf("\n    Cor: %c, Ordem: %i, Minimo: %i e Pai: %i", 
+                    g->A[i].cor, g->A[i].ordem, g->A[i].min, g->A[i].pai);
+        printf("\n\n");
+    }
+}
+
 GRAFO* criarGrafo(int numerodeVertices){
-    printf("Funciona");
+    
     GRAFO* g = (GRAFO*)malloc(sizeof(GRAFO));
     g->vertices = numerodeVertices;
+
+    g->A = (VERTICE*)malloc((numerodeVertices+1)*sizeof(VERTICE));
     
-    g->A = (VERTICE*)malloc(numerodeVertices*sizeof(VERTICE));
-    for(int i=1; i<= numerodeVertices; i++){
+    for(int i = 1; i <= numerodeVertices; i++){
+        g->A[i].numero = i;
         g->A[i].cab = NULL;
         g->A[i].cor = '-';
         g->A[i].min= -1;
@@ -57,87 +74,90 @@ void adicionarAdjacencia(GRAFO* g, int indice, int numeroDoNO){
 }
 
 void adicionarAresta(GRAFO* g, int v, int u){
-    printf("Funfa");
+    
     adicionarAdjacencia(g, v, u);
     adicionarAdjacencia(g, u, v);
 }
 
 
-void DFS(VERTICE* vertice, int v, int* contador){
-    vertice[v].min = vertice[v].ordem;
-    NO* p = vertice[v].cab;
+void DFS(GRAFO* g, int v, int* contador){
+    g->A[v].min = g->A[v].ordem;
+    NO* p = g->A[v].cab;
     
+
     while(p){
         int u = p->numeroDoVertice;
-        if(vertice[u].cor == -1){
+        if(g->A[u].cor == 'b'){
 
-            vertice[u].cor = 'c';
-            vertice[u].pai = v;
-            vertice[u].ordem = ++(*contador);
+            g->A[u].cor = 'c';
+            g->A[u].pai = v;
+            g->A[u].ordem = ++(*contador);
             
-            DFS(vertice, u, contador);
+            DFS(g, u, contador);
         };
 
-        if (vertice[v].min > vertice[u].min && u != vertice[v].pai) 
-            vertice[v].min = vertice[u].min;
+        if (g->A[v].min > g->A[u].min && u != g->A[v].pai) 
+            g->A[v].min = g->A[u].min;
 
         p = p->prox;
     }
-    vertice[v].cor = 'p';
+    g->A[v].cor = 'p';
 };
 
 
 
-bool ehBiconexo(GRAFO* g, char arestaCritica[], int* arestas){
+bool ehBiconexo(GRAFO* g, char arestaCritica[], int* cont){
     int vertices = g->vertices;
     int contador = 1;
 
 
-    VERTICE* vertice = g->A;
 
     //resetar grafo
     for(int i = 1; i <= g->vertices; i++){
-        VERTICE atual = vertice[i];
-        
-        atual.cor = 'b';
-        atual.pai = 0;
-        atual.min = 0;
-        atual.ordem = 0;
+        g->A[i].cor = 'b';
+        g->A[i].pai = 0;
+        g->A[i].min = 0;
+        g->A[i].ordem = 0;
     }
 
 
     int v = 1;
-    vertice[v].cor = 'c';
-    vertice[v].ordem = contador; 
+    g->A[v].cor = 'c';
+    g->A[v].ordem = contador; 
 
-    DFS(vertice, v, &contador);
+    DFS(g, v, &contador);
     
-    vertice[v].cor = 'p';
+    g->A[v].cor = 'p';
 
     if (contador < vertices){ 
-        printf ("NAO EH CONEXO");
+        printf ("NAO EH CONEXO\n");
         return false;
     };
 
-    *arestas = 0;
+    *cont = 0;
     //o primeiro elemento sempre terá o minimo e ordem iguais
     for (int i = 2; i <= vertices; i++){
-        if (vertice[i].min >= vertice[i].ordem){
-            arestaCritica[*arestas] = i + '-' + vertice[i].pai;
-            (*arestas)++;
+        if (g->A[i].min >= g->A[i].ordem){
+            arestaCritica[*cont] = '%i', i;
+            (*cont)++;
+            arestaCritica[*cont] = ',';
+            (*cont)++;
+            arestaCritica[*cont] = '%i', g->A[i].pai;
+            (*cont)++;
+            arestaCritica[*cont] = ' ';
         }; 
     }
 
+    if ((*cont) != 0) return false;
     return true;
 };
 
 
 int main(){
-    printf("Funciona");
 
     GRAFO* gr = criarGrafo(12);
     
-    //printf("Funciona");
+    
     adicionarAresta(gr, 1, 2);
     adicionarAresta(gr, 1, 3);
     adicionarAresta(gr, 2, 4);
@@ -148,23 +168,28 @@ int main(){
     adicionarAresta(gr, 4, 7);
     adicionarAresta(gr, 7, 8);
     adicionarAresta(gr, 7, 9);
-    // adicionarAresta(gr, 7, 12);
-    // adicionarAresta(gr, 8, 9);
-    // adicionarAresta(gr, 9, 10);
-    // adicionarAresta(gr, 9, 11);
-    // adicionarAresta(gr, 10, 11);
-    // adicionarAresta(gr, 10, 12);
+    adicionarAresta(gr, 7, 12);
+    adicionarAresta(gr, 8, 9);
+    adicionarAresta(gr, 9, 10);
+    adicionarAresta(gr, 9, 11);
+    adicionarAresta(gr, 10, 11);
+    adicionarAresta(gr, 10, 12);
 
-    printf("Funciona \n");
-    // char arestaCritica[MAX];
-    // int qntdeDeArestasCriticas;
+  
 
-    // if(ehBiconexo(gr, arestaCritica, &qntdeDeArestasCriticas)) {
-    //     printf ("EH BICONEXO");
-    //     printf ("As  arestas críticas são:");
-    //     // for (int i = 0; i < qntdeDeArestasCriticas; i++)
-    //     //     printf(arestaCritica[i]);
-    // }
+    char arestaCritica[MAX];
+    int qntdeDeArestasCriticas;
+
+    if(!ehBiconexo(gr, arestaCritica, &qntdeDeArestasCriticas)) {
+        printf ("NAO EH BICONEXO\n");
+        printf ("As  arestas críticas são:");
+        for (int i = 0; i < qntdeDeArestasCriticas; i++)
+            printf("%c", arestaCritica[i]);
+        printf("\n");
+    }
+    else printf("EH BICONEXO"); 
+
+    printar(gr);
     
 
     return 0;
